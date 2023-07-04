cd059ab634cac6d6af584b1998f23ed47935ca90
Anderaf <kuldperea@gmail.com>
2023-07-04 23:00:43 +1000

    Fixes

--- SocialNetworkPrototype/MappingProfile.cs
+++ SocialNetworkPrototype/MappingProfile.cs
@@ -1,9 +1,16 @@
 ﻿using AutoMapper;
+using SocialNetworkPrototype.Models.Users;
+using SocialNetworkPrototype.ViewModels.Account;
 
 namespace SocialNetworkPrototype
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile() { }
+        public MappingProfile()
+        {
+            CreateMap<RegisterViewModel, User>()
+                .ForMember(x => x.Email, opt => opt.MapFrom(c => c.EmailReg))
+                .ForMember(x => x.UserName, opt => opt.MapFrom(c => c.FirstName));
+        }
     }
 }
